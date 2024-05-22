da538d2323ec8c943f5db0c9e53cfb94bad8e5cb
Rózsa István <ristvan98@gmail.com>
2024-05-22 22:08:04 +0200

    Fő osztály tesztelésének javítása, JDK csere 17. re és közvetlen osztály megadás annotáción keresztül.

---
+++ src/test/java/FitnessApplicationTests.java
@@ -0,0 +1,9 @@
+import com.example.fitness.FitnessApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = FitnessApplication.class)
+public class FitnessApplicationTests {
+    @Test
+    void contextLoads() {}
+}
